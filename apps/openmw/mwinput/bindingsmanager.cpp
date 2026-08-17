2450e15297117a8403f668b11c53d3b37ebebff0
Caorthann <274241274+caorthann-celt@users.noreply.github.com>
2026-08-17 19:09:19 +0100

    Add console to controller bindings

--- apps/openmw/mwinput/bindingsmanager.cpp
+++ apps/openmw/mwinput/bindingsmanager.cpp
@@ -587,7 +587,7 @@ namespace MWInput
     {
         static const std::initializer_list<int> actions{ A_MoveForward, A_MoveBackward, A_MoveLeft, A_MoveRight,
             A_TogglePOV, A_ZoomIn, A_ZoomOut, A_Sneak, A_Activate, A_Use, A_ToggleWeapon, A_ToggleSpell, A_AutoMove,
-            A_Jump, A_Inventory, A_Journal, A_Rest, A_QuickSave, A_QuickLoad, A_ToggleHUD, A_Screenshot,
+            A_Jump, A_Inventory, A_Journal, A_Rest, A_Console, A_QuickSave, A_QuickLoad, A_ToggleHUD, A_Screenshot,
             A_QuickKeysMenu, A_QuickKey1, A_QuickKey2, A_QuickKey3, A_QuickKey4, A_QuickKey5, A_QuickKey6, A_QuickKey7,
             A_QuickKey8, A_QuickKey9, A_QuickKey10, A_CycleSpellLeft, A_CycleSpellRight, A_CycleWeaponLeft,
             A_CycleWeaponRight };
